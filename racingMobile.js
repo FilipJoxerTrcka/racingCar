4f9341151af2e0554d5040be4e4265328250e0a1
FilipJoxerTrcka <116751051+FilipJoxerTrcka@users.noreply.github.com>
2025-01-07 20:12:33 +0100

    Update racingMobile.js

--- racingMobile.js
+++ racingMobile.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
  
         obstacles.forEach(obstacle => {
-            obstacle.y += 3;
+            obstacle.y += 1;
             if (obstacle.y > canvas.height) {
                 obstacles.shift();
                 score++;
@@ -114,11 +114,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
  
   leftButton.addEventListener("touchstart", () => {
-   velocityX = -5; // Start moving left
+   velocityX = -3; // Start moving left
 });
 
 rightButton.addEventListener("touchstart", () => {
-   velocityX = 5; // Start moving right
+   velocityX = 3; // Start moving right
 });
 
 document.addEventListener("touchend", () => {
